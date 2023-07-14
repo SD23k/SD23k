5810427eed69f71ab64357fa4dbad09ecbca58ca
Shivam diwakar <136838233+SD23k@users.noreply.github.com>
2023-07-14 22:13:25 +0530

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 hi I’m @ Shivam diwakar 
+- 👀 I’m interested in developing 
+- 🌱 I’m currently learning Web development 
+- 💞️ I’m looking to collaborate on websites design 
+- 📫 How to reach me shivamdiwakar528@gmail.com
+
+<!---
+SD23k/Shivam diwakar is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
